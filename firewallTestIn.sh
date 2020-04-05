526d6bba8eb8a2abc26523be5f2d023c81fa7133
adamsalsi <47124506+adamsalsi@users.noreply.github.com>
2020-04-05 11:37:10 -0500

    Update firewallTestIn.sh

--- firewallTestIn.sh
+++ firewallTestIn.sh
@@ -137,14 +137,16 @@ minifirewall --delete 1 &>> $TEST_PATH
 ###############################################################################################
 # Test #10
 printf "\nTest #10 Test Unblock incoming packet policy \n" 2>&1 | tee -a $TEST_PATH
-printf "\nminifirewall --in --srcip 127.0.0.1 --proto TCP --action BLOCK\n" &>> $TEST_PATH
-minifirewall --in --srcip 127.0.0.1 --proto TCP --action BLOCK &>> $TEST_PATH
+printf "\nminifirewall --in --proto TCP --action BLOCK\n" &>> $TEST_PATH
+minifirewall --in --proto TCP --action BLOCK &>> $TEST_PATH
 (sleep 5; echo "Succesfully Recieved the TCP message" | nc -w 1 127.0.0.1 80) | nc -w 10 -l -p 80 &>> $EXTRA_LOGS
 print_result
-printf "\nminifirewall --in --srcip 127.0.0.1 --proto TCP --action UNBLOCK\n" &>> $TEST_PATH
-minifirewall --in --srcip 127.0.0.1 --proto TCP --destport 80 --action UNBLOCK &>> $TEST_PATH
+printf "\nminifirewall --in --proto TCP --action UNBLOCK\n" &>> $TEST_PATH
+minifirewall --in --proto TCP --action UNBLOCK &>> $TEST_PATH
 (sleep 5; echo "Succesfully Recieved the TCP message" | nc -w 1 127.0.0.1 80) | nc -w 10 -l -p 80 &>> $EXTRA_LOGS
 print_result
+printf "\nminifirewall --delete 2\n" &>> $TEST_PATH
+minifirewall --delete 2 &>> $TEST_PATH
 printf "\nminifirewall --delete 1\n" &>> $TEST_PATH
 minifirewall --delete 1 &>> $TEST_PATH
 ###############################################################################################
